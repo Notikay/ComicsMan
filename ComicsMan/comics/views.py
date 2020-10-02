e703cfe54b48af75c4c772c46249202476274f3d
Notikay <a.bob_bis@mail.ru>
2020-10-02 16:03:05 +0500

    Adapted header-slider for mobile devices.

--- ComicsMan/comics/views.py
+++ ComicsMan/comics/views.py
@@ -11,11 +11,12 @@ class ComicsView(View):
         slider = []
         comics = Comics.objects.all()
         slider_list = list(Slide.objects.all())
-        length = len(slider_list)
-        for i in range(0, length, 2):
-            if length % 2 != 0 and i == length - 1:
+        slider_length = len(slider_list)
+        for i in range(0, slider_length, 2):
+            if slider_length % 2 != 0 and i == slider_length - 1:
                 slider.append([slider_list[i]])
             else:
                 slider.append([slider_list[i], slider_list[i + 1]])
         return render(request, "comics/index.html",
-                      {"comics_list": comics, "header_slide_list": slider})
+                      {"comics_list": comics, "header_slide_list": slider,
+                       "slider_length": range(slider_length)})
