34e0b0e35b006e18daea5e68394531f5990fcf8b
Notikay <a.bob_bis@mail.ru>
2020-10-08 04:41:58 +0500

    Added content with new releases to "filters". Changed initialization in "views.py".

--- ComicsMan/comics/views.py
+++ ComicsMan/comics/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.views.generic.base import View
 
@@ -9,8 +11,11 @@ class ComicsView(View):
 
     def __init__(self):
         self.slider = []
-        self.comics_list = list(Comics.objects.all())
+        self.comics = Comics.objects.all()
+        self.comics_list = list(self.comics)
         self.comics_length = len(self.comics_list)
+        self.current_datetime = datetime.now()
+        self.date = f"{self.current_datetime.strftime('%B')} {self.current_datetime.day}: "
 
     def get(self, request):
         for i in range(0, self.comics_length, 2):
@@ -19,5 +24,9 @@ class ComicsView(View):
             else:
                 self.slider.append([self.comics_list[i], self.comics_list[i + 1]])
         return render(request, "comics/index.html",
-                      {"header_slide_list": self.slider,
-                       "comics_length": range(self.comics_length)})
+                      {
+                          "header_slide_list": self.slider,
+                          "comics_length": range(self.comics_length),
+                          "comics_list": self.comics,
+                          "date": self.date
+                      })
