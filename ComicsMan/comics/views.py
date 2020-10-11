40a10fd9c2e0cc821b683be82b4f34decfa93923
Notikay <a.bob_bis@mail.ru>
2020-10-11 20:22:58 +0500

    Added content to "popular" and a function to check the length of an array in "views.py". Centered content and added "new-releases" links

--- ComicsMan/comics/views.py
+++ ComicsMan/comics/views.py
@@ -1,3 +1,5 @@
+# TODO Отдельный список для популярного.
+
 from datetime import datetime
 
 from django.shortcuts import render
@@ -9,35 +11,47 @@ class AppIndexView(View):
     """Для главной станицы."""
 
     def __init__(self):
-        self.slider = []
-
         self.comics = Comics.objects.all()
         self.comics_list = list(self.comics)
-        self.comics_length = len(self.comics_list)
+        self.comics_len = len(self.comics_list)
 
-        self.new_releases_length = 8
+        self.header_slider_len = 8
+        self.new_rel_len = 8
+        self.pop_rel_len = 7
 
         self.current_datetime = datetime.now()
         self.date = f"{self.current_datetime.strftime('%B')} {self.current_datetime.day}: "
 
-    def get(self, request):
-
-        if self.comics_length < self.new_releases_length:
-            new_releases = self.comics_list[:self.comics_length]
+    def checkComics(self, element_len):
+        if self.comics_len < element_len:
+            result = self.comics_list[:self.comics_len]
+            element_len = self.comics_len
         else:
-            new_releases = self.comics_list[:self.new_releases_length]
+            result = self.comics_list[:element_len]
+        return [result, element_len]
 
-        for i in range(0, self.comics_length, 2):
-            if self.comics_length % 2 != 0 and i == self.comics_length - 1:
-                self.slider.append([self.comics_list[i]])
+    def get(self, request):
+        header_slider = []
+        header_slider_list, self.header_slider_len = self.checkComics(self.header_slider_len)
+        new_rel, self.new_rel_len = self.checkComics(self.new_rel_len)
+        pop_rel, self.pop_rel_len = self.checkComics(self.pop_rel_len)
+
+        for i in range(0, self.header_slider_len, 2):
+            if self.header_slider_len % 2 != 0 and i == self.header_slider_len - 1:
+                header_slider.append([header_slider_list[i]])
             else:
-                self.slider.append([self.comics_list[i], self.comics_list[i + 1]])
+                header_slider.append([header_slider_list[i], header_slider_list[i + 1]])
 
         return render(request, "comics/apps/app_index.html",
                       {
-                          "header_slide_list": self.slider,
-                          "comics_length": range(self.comics_length),
+                          "header_slider_len": range(self.header_slider_len),
+                          "header_slider": header_slider,
+
+                          "comics_len": range(self.comics_len),
                           "comics_list": self.comics,
+
+                          "new_rel": new_rel,
+                          "pop_rel": pop_rel,
+
                           "date": self.date,
-                          "new_releases": new_releases,
                       })
