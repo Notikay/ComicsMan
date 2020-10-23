baf7b49326a907fe55138585602648aaac14e97a
Notikay <a.bob_bis@mail.ru>
2020-10-24 00:53:22 +0500

    Added preloader

--- ComicsMan/static/comics/js/scripts.js
+++ ComicsMan/static/comics/js/scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+	$(window).on('load', function () {
+		$('.preloader').fadeOut().end().delay(400).fadeOut('slow');
+		$('body, html').animate({scrollTop: 0}, 0);
+	});
+
 	$(window).on("scroll", function() {
 		if ($(window).scrollTop() > 90) {
 			$(".header .navbar").css("background-color", "#151515");
@@ -11,7 +16,7 @@ $(document).ready(function() {
 		}
 	});
 
-	$("#btn-go-top").on('click', function(){
+	$("#btn-go-top").on('click', function() {
 		$('body, html').animate({scrollTop: 0}, 300);
 		return false;
 	});
